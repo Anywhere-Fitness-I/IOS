09437c3c32e98c64dc00db0bcc5e9c4b125e6dac
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-24 14:16:18 -0700

    Token file created

---
+++ AnywhereFitness/AnywhereFitness/Models/Token.swift
@@ -0,0 +1,13 @@
+//
+//  Token.swift
+//  AnywhereFitness
+//
+//  Created by Bhawnish Kumar on 5/24/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import Foundation
+
+struct Token: Codable {
+    var token: String
+}
