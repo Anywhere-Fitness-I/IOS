fa90012a297c3781d81ca2caba989184052e13cd
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-26 12:24:01 -0700

    Added extension properties for class

--- AnywhereFitness/AnywhereFitness/Models/CoreData/Class+Convenience.swift
+++ AnywhereFitness/AnywhereFitness/Models/CoreData/Class+Convenience.swift
@@ -7,3 +7,33 @@
 //
 
 import Foundation
+import CoreData
+
+
+//name
+//type
+//date
+//startTime
+//duration
+//description
+//intensityLevel
+//location
+//maxClassSize
+extension Class {
+    
+    @discardableResult convenience init(id: Int64, name: String, type: String, date: Date = Date(), startTime: String, duration: String, overview: String, intensityLevel: String, location: String, maxClassSize: Int64, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        self.init(context: context)
+        self.id = id
+        self.name = name
+        self.type = type
+        self.date = date
+        self.startTime = startTime
+        self.duration = duration
+        self.overview = overview
+        self.intensityLevel = intensityLevel
+        self.location = location
+        self.maxClassSize = maxClassSize
+        
+    }
+}
