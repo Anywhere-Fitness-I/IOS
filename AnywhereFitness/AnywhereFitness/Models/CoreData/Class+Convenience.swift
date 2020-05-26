2a9ac93801e49caace92c4c6e595a995e88af5fe
Bharat Kumar <bharatiosdeveloper@gmail.com>
2020-05-26 11:57:28 -0700

    Added Class properties to core data

---
+++ AnywhereFitness/AnywhereFitness/Models/CoreData/Class+Convenience.swift
@@ -0,0 +1,9 @@
+//
+//  Class+Convenience.swift
+//  AnywhereFitness
+//
+//  Created by Bhawnish Kumar on 5/26/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import Foundation
