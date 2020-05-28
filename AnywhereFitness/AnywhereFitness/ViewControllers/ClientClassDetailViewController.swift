8d4e49ef9c8cc6924a3c21fc6144679fa2503b05
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-05-28 14:27:29 -0700

    ClassDetailVieController
    Added labels to corresponding labels

--- AnywhereFitness/AnywhereFitness/ViewControllers/ClientClassDetailViewController.swift
+++ AnywhereFitness/AnywhereFitness/ViewControllers/ClientClassDetailViewController.swift
@@ -9,9 +9,25 @@
 import UIKit
 
 class ClientClassDetailViewController: UIViewController {
+    
+    var backendController = BackendController.shared
+    
+    
 
+    @IBOutlet weak var classNameLabel: UILabel!
+    @IBOutlet weak var classTypeLabel: UILabel!
+    @IBOutlet weak var intensityLabel: UILabel!
+    @IBOutlet weak var instructorNameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var descriptionLaebl: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
 
         // Do any additional setup after loading the view.
     }
