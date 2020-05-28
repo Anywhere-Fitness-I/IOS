643682d7a788bbf53be9db245d23a27e7ec5708f
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-05-28 10:00:24 -0700

    Fixed info.Plist
    fixed info.plist and tableview cell error

---
+++ AnywhereFitness/AnywhereFitness/Views/InstructorTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  InstructorTableViewCell.swift
+//  AnywhereFitness
+//
+//  Created by Enzo Jimenez-Soto on 5/28/20.
+//  Copyright © 2020 Bhawnish Kumar. All rights reserved.
+//
+
+import UIKit
+
+class InstructorTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
