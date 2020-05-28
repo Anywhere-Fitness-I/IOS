70e7290c23c1c6396b47b6e6508b0f2f8de2c471
Enzo Jimenez-Soto <enzojimenezsoto@gmail.com>
2020-05-27 17:04:46 -0700

    CreateAClass
    Create a class Complete

--- AnywhereFitness/AnywhereFitness/CreateAClassView/CreateAClassViewController.swift
+++ AnywhereFitness/AnywhereFitness/CreateAClassView/CreateAClassViewController.swift
@@ -11,18 +11,31 @@ import UIKit
 class CreateAClassViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
     let context = CoreDataStack.shared.mainContext
-
     
+    var backendController = BackendController.shared
+    
+    private let dateFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "en_US")
+        formatter.dateFormat = "MMM d, yyyy"
+        return formatter
+    }()
+    
+    var classSizePicker = UIPickerView()
     var typePicker = UIPickerView()
     var intensityPicker = UIPickerView()
     var durationPicker = UIPickerView()
+    var classSizeData = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10",
+                         "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
+                         "21", "22", "23", "24", "25", "26", "27", "28", "29", "30",
+                         "31", "32", "33", "34", "35", "36", "37", "38", "39", "40"]
     var typeData = ["Choose a workout", "Long Distance Running", "Cycling", "Cardio", "Strength Training", "Yoga", "Sprinting" ]
     var intensityData = ["Choose an intensity", "Light", "Medium", "Hard"]
     var durationData = ["Choose a duration", "20 minutes", "30 minutes", "40 minutes", "50 minutes", "60 minutes", "70 minutes", "80 minutes", "90 minutes" ]
     
-
-     // MARK: - Outlets
-        
+    
+    // MARK: - Outlets
+    
     @IBOutlet private var classNameTextField: UITextField!
     @IBOutlet private var instructorNameTextField: UITextField!
     @IBOutlet private var locationTextField: UITextField!
@@ -34,27 +47,36 @@ class CreateAClassViewController: UIViewController, UIPickerViewDelegate, UIPick
     @IBOutlet private var datePicker: UIDatePicker!
     
     
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-            typePicker.delegate = self
-              intensityPicker.delegate = self
-              typePicker.dataSource = self
-              intensityPicker.dataSource = self
-              durationPicker.delegate = self
-              durationPicker.dataSource = self
-              typePicker.tag = 1
-              intensityPicker.tag = 2
-              durationPicker.tag = 3
-              classTypeTextField.inputView = typePicker
-              intensityLevelTextField.inputView = intensityPicker
-              classDurationTextField.inputView = durationPicker
-              datePicker.minuteInterval = 15
-
+        typePicker.delegate = self
+        intensityPicker.delegate = self
+        typePicker.dataSource = self
+        intensityPicker.dataSource = self
+        durationPicker.delegate = self
+        durationPicker.dataSource = self
+        classSizePicker.delegate = self
+        classSizePicker.dataSource = self
+        typePicker.tag = 1
+        intensityPicker.tag = 2
+        durationPicker.tag = 3
+        classSizePicker.tag = 4
+        maxClassSizeTextField.inputView = classSizePicker
+        classTypeTextField.inputView = typePicker
+        intensityLevelTextField.inputView = intensityPicker
+        classDurationTextField.inputView = durationPicker
+        datePicker.minuteInterval = 15
+        dismissPickerView()
+        
         // Do any additional setup after loading the view.
     }
     
     // MARK: - Methods & Functions
     @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        
+        let dateString = dateFormatter.string(from: datePicker.date)
         
         guard let className = classNameTextField.text, !className.isEmpty else { return }
         guard let instructorName = instructorNameTextField.text, !instructorName.isEmpty else { return }
@@ -64,57 +86,115 @@ class CreateAClassViewController: UIViewController, UIPickerViewDelegate, UIPick
         guard let duration = classDurationTextField.text, !duration.isEmpty else { return }
         guard let maxClassSize = maxClassSizeTextField.text, !maxClassSize.isEmpty else { return }
         guard let classSize = Int64(maxClassSize) else { return }
-        guard let aDescription = classDescriptionTextView.text, !description.isEmpty else { return }
-        let newDate = datePicker.date
+        guard let description = classDescriptionTextView.text, !description.isEmpty else { return }
+        
+        
+        
+        backendController.createClass(name: className, type: type, date: dateString,
+                                      startTime: dateString, duration: duration, description: description, intensityLevel: intensity, location: location, maxClassSize: classSize) { error in
+            if let error = error {
+                NSLog("Error creating Class: \(error)")
+                return
+            }
+            DispatchQueue.main.async {
+                self.showAlertMessage(title: "Created class", message: "Class created", actiontitle: "Ok")
+                //TO DO Perform segue
+            }
+        }
+        
+        do {
+            try CoreDataStack.shared.mainContext.save()
+            
+        } catch {
+            NSLog("Error Saving Class: \(error)")
+            return
+        }
+        navigationController?.dismiss(animated: true, completion: nil)
     }
     
     
-      func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-              if pickerView.tag == 1 {
-                  return typeData.count
-              } else if pickerView.tag == 2 {
-                  return intensityData.count
-              } else {
-                  return durationData.count
-              }
-          }
     
-       func numberOfComponents(in pickerView: UIPickerView) -> Int {
-           return 1
-       }
-       
-        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-             if pickerView.tag == 1 {
-                 return typeData[row]
-             } else if pickerView.tag == 2 {
-                 return intensityData[row]
-             } else {
-                 return durationData[row]
-             }
-         }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 1 {
+            return typeData.count
+        } else if pickerView.tag == 2 {
+            return intensityData.count
+        } else if pickerView.tag == 3 {
+            return durationData.count
+        } else {
+            return classSizeData.count
+        }
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 1 {
+            return typeData[row]
+        } else if pickerView.tag == 2 {
+            return intensityData[row]
+        } else if pickerView.tag == 3 {
+            return durationData[row]
+        } else {
+            return classSizeData[row]
+        }
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        if pickerView.tag == 1 {
+            return  classTypeTextField.text = typeData[row]
+        } else if pickerView.tag == 2 {
+            return intensityLevelTextField.text = intensityData[row]
+        } else if pickerView.tag == 3 {
+            return classDurationTextField.text = durationData[row]
+        } else {
+            return maxClassSizeTextField.text = classSizeData[row]
+        }
+    }
+    
     func dismissPickerView() {
-          let toolBar = UIToolbar()
-          toolBar.sizeToFit()
-          let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dismissKeyboard))
-          toolBar.setItems([doneButton], animated: false)
-          toolBar.isUserInteractionEnabled = true
-          intensityLevelTextField.inputAccessoryView = toolBar
-          classTypeTextField.inputAccessoryView = toolBar
-          classDurationTextField.inputAccessoryView = toolBar
-      }
+        let toolBar = UIToolbar()
+        toolBar.sizeToFit()
+        let doneButton = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(self.dismissKeyboard))
+        toolBar.setItems([doneButton], animated: false)
+        toolBar.isUserInteractionEnabled = true
+        intensityLevelTextField.inputAccessoryView = toolBar
+        classTypeTextField.inputAccessoryView = toolBar
+        classDurationTextField.inputAccessoryView = toolBar
+        maxClassSizeTextField.inputAccessoryView = toolBar
+    }
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
     
+    func showAlertMessage(title: String, message: String, actiontitle: String) {
+        let endAlert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let endAction = UIAlertAction(title: actiontitle, style: .default) { (action: UIAlertAction ) in
+        }
+        endAlert.addAction(endAction)
+        present(endAlert, animated: true, completion: nil)
+    }
+    
+    func updateViews() {
+        
+        
+    }
+    
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
+
+
